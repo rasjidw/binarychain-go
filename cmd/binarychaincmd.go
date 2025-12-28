36e6e8810f3ca094b7c8bd757c427c3f74d64bc5
Rasjid Wilcox <rasjidw@openminddev.net>
2025-12-28 15:36:17 +1100

    Use non-streaming reader.

--- cmd/binarychaincmd.go
+++ cmd/binarychaincmd.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	prefix := "A123"
 	parts := []([]byte){[]byte{1, 2, 3, 4}, []byte{0x81, 0x82, 0x83, 0x90, 0x91}}
+	parts = append(parts, []byte{0xff})
 	bc, err := binarychain.NewBinaryChain(&prefix, &parts)
 	if err != nil {
 		fmt.Printf("Invalid binary chain: %v", err)
@@ -24,6 +25,17 @@ func main() {
 			fmt.Printf("Got Error: %v\n", item.ItemErr)
 		} else {
 			fmt.Printf("Got: %v\n", item.Result)
+		}
+	}
+
+	fmt.Print("---- end of streaming reader\n")
+
+	cr := binarychain.NewChainReader()
+	for bc_result := range cr.GetBinaryChains(*bc_data) {
+		if bc_result.ErrResult != nil {
+			fmt.Printf("Got BC Error: %v\n", bc_result.ErrResult)
+		} else {
+			fmt.Printf("Got BinChain: %v\n", bc_result.BinChain)
 		}
 	}
 
@@ -59,9 +71,6 @@ func main() {
 			fmt.Printf("Got Binary Part: %v\n", v)
 		case *binarychain.EndOfChainMarker:
 			fmt.Printf("Got EOC Marker: %v\n", v)
-		case *binarychain.ParseError:
-			// should never get this due to above
-			fmt.Printf("Got parse error: %v", v)
 		default:
 			fmt.Printf("Default case: Got %v\n", v)
 		}
